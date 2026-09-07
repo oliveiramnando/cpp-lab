abad0e5676ec6d962666790d39e65e895a058673
oliveiramnando <oliveiramnando@gmail.com>
2026-09-07 15:36:35 -0400

    Multiply by 2

---
+++ learncpp/chapter-01/multiplyByTwo/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+
+int main()
+{
+    int x{};
+
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+
+    std::cout << "Double that number is: " << x * 2 << '\n';
+
+    return 0;
+}
+
